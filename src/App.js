9b82127eed8f2a7ed71270e029cc57380ba7b7d8
Adnan <Adnan@DESKTOP-UGPNVH0>
2023-05-10 16:47:31 +0600

    Styled navbar active/ inactive

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './pages/Home'
-import About from './pages/About'
-import Products from './pages/Products'
-import Error from './pages/Error'
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
+import Error from "./pages/Error";
+import Dashboard from './pages/Dashboard';
+import SharedLayout from "./pages/SharedLayout";
+
 
 function App() {
   return (
     <BrowserRouter>
-    <nav>Our Nav</nav>
+      {/* <nav>Our Nav</nav> */}
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="products" element={<Products />} />
-        <Route path="*" element={<Error />}/>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="products" element={<Products />} />
+          <Route path="*" element={<Error />} />
+        </Route>
       </Routes>
-      <footer>Our Footer</footer>
+      {/* <footer>Our Footer</footer> */}
     </BrowserRouter>
   );
 }
