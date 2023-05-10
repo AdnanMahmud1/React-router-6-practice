9064681f6463c9bc6079b47157536a764134ee4f
Adnan <Adnan@DESKTOP-UGPNVH0>
2023-05-10 17:02:05 +0600

    Single product showcase

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,12 +1,14 @@
 import { Link,useParams } from 'react-router-dom';
-
+import products from '../data'
 
 const SingleProduct = () => {
   const {productId} = useParams();
-
+  const product = products.find((product)=> product.id === productId);
+  const {image, name} = product
   return (
     <section className='section product'>
-      <h4>single product code {productId}</h4>
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
       <Link to="/products">back to all products</Link>
     </section>
   );
