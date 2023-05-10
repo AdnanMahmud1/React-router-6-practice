11e96da83b663fe2c7df60a55a8996e734ec22ea
DESKTOP-NOMKETR\adnan <11adnan@live.com>
2023-05-11 00:22:24 +0600

    Dummy login to dshboard created

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import About from "./pages/About";
 import Products from "./pages/Products";
 import Error from "./pages/Error";
 import Dashboard from './pages/Dashboard';
+import Login from "./pages/Login";
 import SharedLayout from "./pages/SharedLayout";
 import SingleProduct from './pages/SingleProduct';
+import { useState } from "react";
 
 
 function App() {
+  const [user, setUser]=useState(null)
   return (
     <BrowserRouter>
       {/* <nav>Our Nav</nav> */}
@@ -18,6 +21,8 @@ function App() {
           <Route path="about" element={<About />} />
           <Route path="products" element={<Products />} />
           <Route path="products/:productId" element={<SingleProduct />} />
+          <Route path="login" element={<Login setUser={setUser} />} />
+          <Route path="dashboard" element={<Dashboard user={user} />} />
           <Route path="*" element={<Error />} />
         </Route>
       </Routes>
